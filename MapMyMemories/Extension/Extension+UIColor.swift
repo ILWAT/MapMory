8dbf92bef1786ec3af37aaa439cefdf722620fb4
J.H. Moon <moonjh587@naver.com>
2023-09-25 20:38:06 +0900

    [Setting]-#1 초기 폴더링 & 개발 환경 셋팅

---
+++ MapMyMemories/Extension/Extension+UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+UIColor.swift
+//  MapMyMemories
+//
+//  Created by 문정호 on 2023/09/25.
+//
+
+import UIKit
+
+extension UIColor{
+    static let mainBackgroundColor = UIColor(named: "MainBackgroundColor")
+    
+    static let mainTintColor = UIColor(named: "MainTintColor")
+}
