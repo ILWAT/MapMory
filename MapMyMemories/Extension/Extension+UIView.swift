37249b2f671435519c81d73cdf8b001c1bdbbebb
J.H. Moon <moonjh587@naver.com>
2023-10-09 02:44:15 +0900

    [Add] 개발에 필요한 Extension 구성

--- MapMyMemories/Extension/Extension+UIView.swift
+++ MapMyMemories/Extension/Extension+UIView.swift
@@ -9,11 +9,17 @@ import UIKit
 
 
 extension UIView{
+    static var identifier: String{
+        get{
+            return String(describing: self)
+        }
+    }
     
     func addSubViews(_ subViews: [UIView]){
         subViews.forEach { element in
             self.addSubview(element)
         }
     }
+    
 }
 
