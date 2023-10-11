a1918a9ec1c09a2708f204f3a96e1ac17852f56e
J.H. Moon <moonjh587@naver.com>
2023-10-11 23:50:56 +0900

    [Refactor] - #8 Observable 클래스 생성 및 WriteViewModel 생성

---
+++ MapMyMemories/Scene/ViewModel/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  MapMyMemories
+//
+//  Created by 문정호 on 2023/10/11.
+//
+
+import Foundation
+
+class Observable<T>{
+    private var notifier: ((T)->Void)?
+    
+    var value: T{
+        didSet{
+            notifier?(value)
+        }
+    }
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T)->Void){
+        self.notifier = closure
+    }
+    
+    
+}
